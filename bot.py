9faea16e3eddd3d127a4ec96eb1a7cb9c62bb98a
qijie44 <qijie44@gmail.com>
2022-08-19 19:53:38 +0800

    added multiple spins, deleting when the command is run after 120s have elapsed

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ from telegram.ext import MessageHandler, Filters
 import configparser
 import logging
 import random
-
+import datetime
 
 config = configparser.ConfigParser()
 config.read("bot.ini")
+data = {}
 
 updater = Updater(token=config["KEYS"]["BOT_TOKEN"], use_context="true")
 dispatcher = updater.dispatcher
@@ -20,22 +21,28 @@ def start(update, context):
 
 def spin_the_wheel(update, context):
     #instantiate an empty array with the chat id
-    globals()[update.effective_chat.id]= []
+    data[update.effective_chat.id]= [[], 0]
     context.bot.send_message(chat_id= update.effective_chat.id, text="Wheel started! Please type in the choices.")
 
 def text(update, context):
     # if the array exists, add to it and inform the user
-    if update.effective_chat.id in globals():
-        globals()[update.effective_chat.id].append(update.message.text)
+    if update.effective_chat.id in data:
+        data[update.effective_chat.id][0].append(update.message.text)
         message = update.message.text + " added!"
         context.bot.send_message(chat_id= update.effective_chat.id, text=message)
 
 def spin(update, context):
-    # returns a random choice from the list and delete the list
-    choice = random.choice(globals()[update.effective_chat.id])
-    message = choice + " was selected!"
-    del globals()[update.effective_chat.id]
-    context.bot.send_message(chat_id= update.effective_chat.id, text=message)
+    if update.effective_chat.id in data:
+        # returns a random choice from the list and delete the list
+        choice = random.choice(data[update.effective_chat.id][0])
+        data[update.effective_chat.id][1] = datetime.datetime.now()
+        message = choice + " was selected!"
+        context.bot.send_message(chat_id= update.effective_chat.id, text=message)
+        for user in data:
+            if ((datetime.datetime.now() - data[user][1]).seconds) >= 120:
+                del data[user]
+    else:
+        context.bot.send_message(chat_id= update.effective_chat.id, text="Please run the /spin_the_wheel command to start a wheel!")
 
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("spin_the_wheel", spin_the_wheel))
